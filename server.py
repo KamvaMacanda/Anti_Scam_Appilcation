94e24c30ede9f2ef63e84ec18becc11e433279e2
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2025-09-13 01:25:02 +0200

    creating server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver 
+import socket 
+
+PORT = 8000 
+Handler = http.server.SimpleHTTPRequestHandler 
+hostname = socket.gethostname() 
+local_ip = socket.gethostbyname(hostname) 
+
+with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd: 
+    print(f"Serving at http://localhost:{local_ip}:{PORT}") 
+    print(f"Or on local network http://{local_ip}:{PORT}") 
+    httpd.serve_forever()
+
+
