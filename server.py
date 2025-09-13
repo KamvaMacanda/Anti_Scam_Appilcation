209101810fbfcde38d054c50d18aa57ff048642c
KamvaMacanda-St10496666 <st10496666@vcconnect.edu.za>
2025-09-13 04:25:58 +0200

    fixed valiation method

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver 
 import socket 
 
-PORT = 8000 
+PORT = 8002
 Handler = http.server.SimpleHTTPRequestHandler 
 
 hostname = socket.gethostname() 
@@ -12,5 +12,6 @@ with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
     print(f"Serving at http://localhost:{PORT}") 
     print(f"Or on local network: http://{local_ip}:{PORT}") 
     httpd.serve_forever() 
+    
 
 
